2940fa7756b101bcb168e935eb266ce4c8210b9a
Ign22 <ignholdings@gmail.com>
2025-08-05 23:07:23 +0300

    Add real Android app so Appflow can build it

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,9 @@
-import { CapacitorConfig } from '@capacitor/core';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.b209ff8783df4a0fa8a21b0230f186dc',
-  appName: 'HabitHive',
-  webDir: 'dist',
-  server: {
-    url: 'https://b209ff87-83df-4a0f-a8a2-1b0230f186dc.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0
-    }
-  }
+  appId: 'com.habithive',
+  appName: 'habithive',
+  webDir: 'dist'
 };
 
 export default config;
